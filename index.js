54a34017fac7ff87c60ce5c724766e2e2bbb8c16
HEMLEEN <101266244+HEMLEEN@users.noreply.github.com>
2022-04-02 23:37:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function myfunc(){
+	document.getElementById("demo").innerHTML="Document object model.";
+	
+}
+function fnOperation(){
+	var value1 = prompt("Eneter a value 1");
+	var value2 = prompt("Eneter a value 2");
+	var Result = parseInt(value1) + parseInt(value2);
+	alert("Result is " + Result);
+}
